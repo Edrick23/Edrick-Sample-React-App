8d63befd5b88590412f3d24a9e49456f0e9a606f
Edrick Lomibao <lomibaoedrick@outlook.com>
2019-07-03 17:13:30 +0800

    no message

---
+++ src/containers/LoginPage/LoginPage.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { userActions } from '../../actions';
+import * as S from './style';
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.dispatch(userActions.logout());
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        const { dispatch } = this.props;
+        if (username && password) {
+            dispatch(userActions.login(username, password));
+        }
+    }
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
+        return (
+
+        <S.Wrapper>
+            <S.BannerWrapper>
+                <S.Banner/>
+            </S.BannerWrapper>
+
+            <div className="col-sm-8 col-sm-offset-2">
+            {alert.message &&
+            <div className={`alert ${alert.type}`}>{alert.message}</div>
+             }
+                        
+            </div>
+            
+                <S.Container className="col-md-3 col-md-offset-4">
+                    
+                    <form name="form" onSubmit={this.handleSubmit}>
+
+                        <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                            <label htmlFor="username">Username</label>
+                            <S.Input type="text" 
+                                className="form-control form-control-sm" 
+                                name="username" 
+                                value={username} 
+                                onChange={this.handleChange}>
+                            </S.Input>
+                            {submitted && !username &&
+                                <div className="help-block">Username is required</div>
+                            }
+                        </div>
+                        <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                            <label htmlFor="password">Password</label>
+                            <S.Input type="password" 
+                            className="form-control form-control-sm" 
+                            name="password" 
+                            value={password} 
+                            onChange={this.handleChange} />
+                            {submitted && !password &&
+                                <div className="help-block">Password is required</div>
+                            }
+                        </div>
+                        <div className="form-group">
+                            <button className="btn btn-primary btn-block">Login</button>
+                            {loggingIn &&
+                                <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
+                            }
+                        </div>
+                    </form>
+                </S.Container>
+
+        </S.Wrapper>
+            
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const { loggingIn, alert } = state;
+    return {
+        loggingIn,
+        alert 
+    };
+}
+
+const connectedLoginPage = connect(mapStateToProps)(LoginPage);
+export { connectedLoginPage as LoginPage }; 
---
+++ src/containers/LoginPage/style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: white;
+`;
+
+export const BannerWrapper = styled.div`
+    position: relative;
+    min-height: 100px;
+
+`;
+
+export const Banner = styled.div`
+    height:50px;
+    Background-color:#3498DB;
+`;
+
+export const Input = styled.input`
+  border-Color:1px solid #c8c8c8;
+  -webkit-box-shadow: none !important;
+  -moz-box-shadow: none !important;
+  box-shadow: none !important;
+  outline: none;
+`;
+
+export const Container = styled.div`
+  Margin-top:30px;
+`;
+
+export const Footer = styled.div`
+  Margin-top:30px;
+  border-top:1px solid #c8c8c8;
+  min-height:50px;
+`;
